a77b9c4c607e6c01b48f15f7e51548d403fbfa21
Aritra Talukdar <aritratalukdar01@gmail.com>
2026-01-09 13:39:07 +0530

    Update fit_model.py

--- python/fit_model.py
+++ python/fit_model.py
@@ -1,7 +1,14 @@
-def fit_model(ns, ts, f):
-    num = sum(ts[i] * f(ns[i]) for i in range(len(ns)))
-    den = sum(f(ns[i]) ** 2 for i in range(len(ns)))
-    a = num / den
+import numpy as np
 
-    error = sum((ts[i] - a * f(ns[i])) ** 2 for i in range(len(ns)))
+def fit_model(ns, ts, f):
+    ns = np.array(ns)
+    ts = np.array(ts)
+    
+    # Scale factor 'a' using least squares
+    f_values = f(ns)
+    a = np.sum(ts * f_values) / np.sum(f_values ** 2)
+    
+    # Compute squared error
+    error = np.sum((ts - a * f_values) ** 2)
     return error
+
